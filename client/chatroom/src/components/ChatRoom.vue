97d1ddbea93357817aa736a69554aa568af672c0
Bar Snir <bars@Admins-MacBook-Pro.local>
2020-02-05 21:08:58 +0200

    added client + div for chat example

---
+++ client/chatroom/src/components/ChatRoom.vue
@@ -0,0 +1,27 @@
+<template>
+    <div id="chatRoom">
+      
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'ChatRoom',
+  props: {
+    msg: String
+  },
+  methods :{
+
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  #chatRoom {
+    margin: 15px auto;
+    border: 1px solid black;
+    width: 500px;
+    height: 80vh;
+  }
+</style>
