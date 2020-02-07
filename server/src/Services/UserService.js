daf1d1d1cbcc244e3f76f8d8a0ecac067cf4c726
Bar Snir <bars@Admins-MacBook-Pro.local>
2020-02-07 14:05:59 +0200

    added service to server

---
+++ server/src/Services/UserService.js
@@ -0,0 +1,37 @@
+const importedUsers = require('../userList').userList;
+const UserEvents = require('../DataSource/UserEvents').modules;
+const Observables = require('../Observables/UserObservable').modules;
+const UserRepo = require('../Repositories/UserRepo').modules;
+
+class UserService {
+    constructor() {
+        this.userPosition = 0;
+        this.userList = Object.assign(importedUsers, []);
+        this.userEvents = UserEvents;
+        this.userRepo = UserRepo;
+    }
+
+    addUsers() {
+        this.userEvents.addUserEvent(this.userList[this.userPosition]);
+        this.userRepo.emitUser(this.userList[this.userPosition], this.userEvents.actionDisplay);
+        for (let observer in Observables.observerList) {
+            this.userRepo.emitMessage(Observables.observerList[observer].userMessage)
+        }
+        this.userPosition++;
+        return this.userPosition === this.userList.length;
+    }
+
+    removeUsers() {
+        this.userEvents.removeUserEvent(this.userList[this.userList.length-1]);
+        this.userRepo.emitUser(this.userList[this.userList.length-1], this.userEvents.actionDisplay);
+        this.userList.pop();
+        for (let observer in Observables.observerList) {
+            this.userRepo.emitMessage(Observables.observerList[observer].userMessage)
+        }
+        this.userPosition++;
+        return !this.userList.length;
+    }
+
+}
+
+exports.modules = new UserService();
--- server/src/controllers/UserController.js
+++ server/src/controllers/UserController.js
@@ -1,45 +1,21 @@
-const importedUsers = require('../userList').userList;
-const UserEvents = require('../DataSource/UserEvents').modules;
-const Observables = require('../Observables/UserObservable').modules;
-const UserRepo = require('../Repositories/UserRepo').modules;
-const WebSocketClient = require('../DataSource/WebSocketClient').modules;
-
+const UserService = require('../Services/UserService').modules
 
 class UserController {
     constructor() {
-        this.userPosition = 0;
-        this.userList = Object.assign(importedUsers, []);
-        this.userEvents = UserEvents;
-        this.userRepo = UserRepo;
-        this.webSocketClient = WebSocketClient;
+        this.userService = UserService;
     }
-
     addUsers(req, res, next) {
         const addEventInterval = setInterval(()=>{
-            this.userEvents.addUserEvent(this.userList[this.userPosition]);
-            this.userRepo.emitUser(this.userList[this.userPosition], this.userEvents.actionDisplay);
-            for (let observer in Observables.observerList) {
-                this.userRepo.emitMessage(Observables.observerList[observer].userMessage)
-            }
-            this.userPosition++;
-            if(this.userPosition === this.userList.length){
+            if(this.userService.addUsers()){
                 clearInterval(addEventInterval);
                 res.end();
                 return;
             }
         },2000); 
     }
-
     removeUsers(req, res, next) {
         const removeEventInterval = setInterval(()=> {
-            this.userEvents.removeUserEvent(this.userList[this.userList.length-1]);
-            this.userRepo.emitUser(this.userList[this.userList.length-1], this.userEvents.actionDisplay);
-            this.userList.pop();
-            for (let observer in Observables.observerList) {
-                this.userRepo.emitMessage(Observables.observerList[observer].userMessage)
-            }
-            this.userPosition++;
-            if(!this.userList.length){
+            if(this.userService.removeUsers()){
                 clearInterval(removeEventInterval);
                 res.end();
                 return;
